6b858940f5410930c654093ee0349ca028e0a819
Felix <felixfbpuppy@gmail.com>
2020-03-15 19:50:23 -0400

    removed linebreaks from youtube-dl updater log

--- services/youtubedlup.py
+++ services/youtubedlup.py
@@ -11,7 +11,7 @@ out = outb.decode()
 #output = outl[0]
 
 strline = "Ran at " + time.asctime() + " Eastern Time: "
-strwrite = strline + out + "\n"
+strwrite = strline + out.replace("\n","") + "\n"
 f = open(logfile, "a")
 f.write(strwrite)
 f.close()
